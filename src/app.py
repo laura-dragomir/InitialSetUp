75fdd390cd97753d14d85a0ae5d54b7badae4400
laura-dragomir <laura.dragomir01@e-uvt.ro>
2024-11-06 17:52:12 +0200

    Add files via upload

---
+++ src/app.py
@@ -0,0 +1,23 @@
+# app.py
+def add(a, b):
+    """
+    Add two numbers together.
+
+    :param a: First number
+    :param b: Second number
+    :return: Sum of a and b
+    """
+    return a + b
+
+def multiply(a, b):
+    """
+    Multiply two numbers together
+
+    Args:
+        a (int): First number
+        b (int): Seconds number
+
+    Returns:
+        (int): Product of a and b
+    """
+    return a * b
